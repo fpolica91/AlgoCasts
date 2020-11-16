95a07fc3aa312a8f5cbfba36c6ba795f18c69710
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-11-15 23:04:50 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+  let max_char = 0
+  let max = ""
+  for(let char of str){
+    chars[char] = chars[char] + 1 || 1;
+  }
+  for(let char in chars){
+    if(chars[char] > max_char){
+      max_char = chars[char]
+      max = char
+    }
+  }
+  return max
+}
 
 module.exports = maxChar;
